e89d86ee3ec3d23162ab8f2a945174c210ed0748
unknown <joeleewiserobot@gmail.com>
2021-02-08 17:51:40 +0800

    add checking condition in access module

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -29,108 +29,114 @@ let Module19 = true
 let Module20 = true
 
 
-if(access[0].Chk != 'Y'){
-  Module1 = true
-}else{
-  Module1 = false
-}
-if(access[1].Chk != 'Y'){
-  Module2 = true
-}else{
-  Module2 = false
-}
-if(access[2].Chk != 'Y'){
-  Module3 = true
-}else{
-  Module3 = false
-}
-if(access[3].Chk != 'Y'){
-  Module4 = true
-}else{
-  Module4 = false
-}
-if(access[4].Chk != 'Y'){
-  Module5 = true
-}else{
-  Module5 = false
-}
-if(access[5].Chk != 'Y'){
-  Module6 = true
-}else{
-  Module6 = false
-}
-if(access[6].Chk != 'Y'){
-  Module7 = true
-}else{
-  Module7 = false
-}
-if(access[7].Chk != 'Y'){
-  Module8 = true
-}else{
-  Module8 = false
-}
-if(access[8].Chk != 'Y'){
-  Module9 = true
-}else{
-  Module9 = false
-}
-if(access[9].Chk != 'Y'){
-  Module10 = true
-}else{
-  Module10 = false
-}
-if(access[10].Chk != 'Y'){
-  Module11 = true
-}else{
-  Module11 = false
-}
-if(access[11].Chk != 'Y'){
-  Module12 = true
-}else{
-  Module12 = false
-}
-if(access[12].Chk != 'Y'){
-  Module13 = true
-}else{
-  Module13 = false
-}
-if(access[13].Chk != 'Y'){
-  Module14 = true
-}else{
-  Module14 = false
-}
-if(access[14].Chk != 'Y'){
-  Module15 = true
-}else{
-  Module15 = false
-}
-if(access[15].Chk != 'Y'){
-  Module16 = true
-}else{
-  Module16 = false
-}
-if(access[16].Chk != 'Y'){
-  Module17 = true
-}else{
-  Module17 = false
-}
-if(access[17].Chk != 'Y'){
-  Module18 = true
-}else{
-  Module18 = false
-}
-if(access[18]!= null){
-  if(access[18].Chk != 'Y'){
-    Module19 = true
+
+if (access) {
+
+  if(access[0].Chk != 'Y'){
+    Module1 = true
   }else{
-    Module19 = false
+    Module1 = false
+  }
+  if(access[1].Chk != 'Y'){
+    Module2 = true
+  }else{
+    Module2 = false
+  }
+  if(access[2].Chk != 'Y'){
+    Module3 = true
+  }else{
+    Module3 = false
+  }
+  if(access[3].Chk != 'Y'){
+    Module4 = true
+  }else{
+    Module4 = false
+  }
+  if(access[4].Chk != 'Y'){
+    Module5 = true
+  }else{
+    Module5 = false
+  }
+  if(access[5].Chk != 'Y'){
+    Module6 = true
+  }else{
+    Module6 = false
+  }
+  if(access[6].Chk != 'Y'){
+    Module7 = true
+  }else{
+    Module7 = false
+  }
+  if(access[7].Chk != 'Y'){
+    Module8 = true
+  }else{
+    Module8 = false
+  }
+  if(access[8].Chk != 'Y'){
+    Module9 = true
+  }else{
+    Module9 = false
+  }
+  if(access[9].Chk != 'Y'){
+    Module10 = true
+  }else{
+    Module10 = false
+  }
+  if(access[10].Chk != 'Y'){
+    Module11 = true
+  }else{
+    Module11 = false
+  }
+  if(access[11].Chk != 'Y'){
+    Module12 = true
+  }else{
+    Module12 = false
+  }
+  if(access[12].Chk != 'Y'){
+    Module13 = true
+  }else{
+    Module13 = false
+  }
+  if(access[13].Chk != 'Y'){
+    Module14 = true
+  }else{
+    Module14 = false
+  }
+  if(access[14].Chk != 'Y'){
+    Module15 = true
+  }else{
+    Module15 = false
+  }
+  if(access[15].Chk != 'Y'){
+    Module16 = true
+  }else{
+    Module16 = false
+  }
+  if(access[16].Chk != 'Y'){
+    Module17 = true
+  }else{
+    Module17 = false
+  }
+  if(access[17].Chk != 'Y'){
+    Module18 = true
+  }else{
+    Module18 = false
+  }
+  if(access[18]!= null){
+    if(access[18].Chk != 'Y'){
+      Module19 = true
+    }else{
+      Module19 = false
+    }
+  }
+  if(access[19]!= null){
+    if(access[19].Chk != 'Y'){
+      Module20 = true
+    }else{
+      Module20 = false
+    }
   }
 }
-if(access[19]!= null){if(access[19].Chk != 'Y'){
-  Module20 = true
-}else{
-  Module20 = false
-}}
 
 
 export const MENU_ITEMS: NbMenuItem[] = [
