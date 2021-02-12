c0aa38fcda29899a0d28d799ad3b09c63ffe6f64
Hakim <hakim@wiserobot.com>
2021-02-12 08:27:21 +0800

    Fix bug unable to export matrix vertical data

--- src/app/pages/report/matrix.component.ts
+++ src/app/pages/report/matrix.component.ts
@@ -109,7 +109,6 @@ export class MatrixComponent implements OnDestroy, OnInit {
       const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.newTableDetails.dict);
       const wb: XLSX.WorkBook = XLSX.utils.book_new();
       XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-      XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
       XLSX.writeFile(wb, 'matrix.xlsx');
     }
   }
